98ad1620c07fd06b290548eb48fc9e3480619896
Wagner Almeida <wdmeida@gmail.com>
2018-03-07 15:05:05 -0300

    Add bgColor custom

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropType from 'prop-types';
 
+const defaultProps = {
+  bgColor: '#ccc',
+};
+
 const propTypes = {
   title: PropType.string,
   subtitle: PropType.string,
 };
 
-const FullHeader = ({ title, subtitle }) => {
+const FullHeader = ({ title, subtitle, bgColor }) => {
+  const headerStyles = {
+    backgroundColor: bgColor,
+  };
+
   const component = (
-    <header>
+    <header style={headerStyles}>
       {title && <h1>{title}</h1>}
       {subtitle && <h2>{subtitle}</h2>}
     </header>
@@ -18,5 +26,6 @@ const FullHeader = ({ title, subtitle }) => {
 };
 
 FullHeader.propTypes = propTypes;
+FullHeader.defaultProps = defaultProps;
 
 export default FullHeader;
--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
 import FullHeader from '../../src/Main';
+
+chai.use(chaiEnzyme());
 
 describe('<FullHeader />', () => {
   it('should have header tag when mount', () => {
@@ -40,6 +43,18 @@ describe('<FullHeader />', () => {
     it('should have h2 tag with the subtitle passed', () => {
       const wrapper = shallow(<FullHeader subtitle="Curso" />);
       expect(wrapper.find('h2').props().children).to.equal('Curso');
+    });
+  });
+
+  context('bgColor', () => {
+    it('should have background-color equal #ccc when none is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" />);
+      expect(wrapper).to.have.style('background-color').equal('#ccc');
+    });
+
+    it('should have background-color equal #000 when none is passed', () => {
+      const wrapper = shallow(<FullHeader title="TDD" bgColor="#000" />);
+      expect(wrapper).to.have.style('background-color').equal('#000');
     });
   });
 });
